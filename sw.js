64b1b724b5c4b4361dbc27a5cba0d3ded7ab3761
Максим Добычин <dobyvankenoby@gmail.com>
2026-09-13 12:20:14 +0300

    🔊 Аудиосубтитры: герои говорят вслух своими голосами (задания не читаются)

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Scoped offline application updater; version changes with the payload and updater.
-const VERSION = '509f2257b3';
+const VERSION = 'd4d02ce0de';
 const PREFIX = "bitva-";
 const CACHE = PREFIX + VERSION;
 const SCOPE = new URL(self.registration.scope);
